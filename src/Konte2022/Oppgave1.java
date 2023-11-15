3e430d39aedc842c55e7fee91f979c008ebcf872
huseyindavut <saskin@protonmail.com>
2023-11-15 23:57:27 +0100

    az daha cslis bakalim :)

--- src/Konte2022/Oppgave1.java
+++ src/Konte2022/Oppgave1.java
@@ -1,11 +1,19 @@
 package Konte2022;
 
 public class Oppgave1 {
+    public static void main(String[] args) {
+
+       for (int i =10; i>=-10; i--){
+           if (i != -10){
+               System.out.print(i+", ");
+           }else{
+               System.out.print(i+".");
+           }
+       }
+    }
+
+
 }
-
-
-
-
 
 /*
 a) Skriv kode for å summere hvert 10. tall fra 0 til 1000, dvs. 10,20,30 .... 990, 1000 ved hjelp av en for-løkke.
